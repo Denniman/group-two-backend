c61d8d2de7be6751985abf8ee19cceeb00aed9df
denniman <dennimanadams@gmail.com>
2024-04-15 02:06:19 +0100

    chore: fi linting issues

--- typings/express/index.d.ts
+++ typings/express/index.d.ts
@@ -4,12 +4,12 @@ import { User, Token } from "../user";
 import { FormImageType } from "typings/image";
 
 declare global {
-    export namespace Express {
-        interface Request {
-            token: UserTokenType;
-            uploadedImageFile?: string;
-            file?: { [key: string]: FormImageType };
-            paginate: PaginateFunction;
-        }
+  export namespace Express {
+    interface Request {
+      token: UserTokenType;
+      uploadedImageFile?: string;
+      file?: { [key: string]: FormImageType };
+      paginate: PaginateFunction;
     }
+  }
 }
